7309c3b9cf73fe9f1720aee7b7344b37348bc5b9
Mayar <mayar.alidib@alternativa.dev>
2024-12-06 05:51:13 +0400

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+
+
+      <h1 className=" bg-red-500 text-black font-bold text-4xl">
+        jojo,loar
+      </h1>
+      Hi
+
+
+    </div>
+  );
+}
