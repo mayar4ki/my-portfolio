ff53f6d6b0c965693bbf85596438b04c6ddc994d
Mayar <mayar.alidib@alternativa.dev>
2024-12-21 20:33:04 +0400

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   images: {
@@ -9,13 +8,7 @@ const nextConfig: NextConfig = {
         hostname: "**",
       },
     ],
-    unoptimized: true, // Disable default image optimization
-  },
-  reactStrictMode: true,
-  assetPrefix: isProd ? '/my-portfolio/' : '',
-  basePath: isProd ? '/my-portfolio' : '',
-  output: 'export'
-
+  }
 };
 
 export default nextConfig;
