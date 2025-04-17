12a6e8de9f3b744f8cba9a9cb08b99fc22a48e0c
Mayar Deeb <mayar.e.deeb@gmail.com>
2025-04-17 19:31:02 +0400

    update data

--- src/screens/home/components/tech.tsx
+++ src/screens/home/components/tech.tsx
@@ -3,55 +3,68 @@
 import Image from "next/image";
 import { Typography } from "@material-tailwind/react";
 
-const FRAMEWORKS = [
-  "react",
-  "next",
-  "react-native"
-];
-
+const FRAMEWORKS = ["react", "next", "react-native"];
 
 const LIBS = [
   {
     title: "Validation & Forms",
-    values: ["Yup", "JOI", "Zod", "Formik", "Hook Form"]
+    values: ["Yup", "JOI", "Zod", "Formik", "Hook Form"],
   },
   {
     title: "State Management",
-    values: ["Redux", "Redux-toolkit", "Zustand", "Context Api", "Rxjs"]
+    values: ["Redux", "Redux-toolkit", "Zustand", "Context Api", "Rxjs"],
   },
   {
     title: "Maps",
-    values: ["Google Map", "Mapbox", "Baidu", "Leaflet", "Deck.js", "2gis"]
+    values: ["Google Map", "Mapbox", "Baidu", "Leaflet", "Deck.js", "2gis"],
   },
   {
     title: "Ui-Web",
-    values: ["MUI", "Ant-design", "Mantine", "Gluestack", "Native-base", "Tailwind"]
+    values: [
+      "MUI",
+      "Ant-design",
+      "Mantine",
+      "Gluestack",
+      "Native-base",
+      "Tailwind",
+    ],
   },
   {
     title: "Ui-Mobile",
-    values: ["Gluestack", "Native-base", "React Native Paper", "shopify/restyle"]
+    values: [
+      "Gluestack",
+      "Native-base",
+      "React Native Paper",
+      "shopify/restyle",
+    ],
   },
   {
     title: "Communication",
-    values: ["React Query (Rest APi)", "Apollo (Graph QL)", "Socket.IO", "Pusher", "Fire base"]
+    values: [
+      "React Query (Rest APi)",
+      "Apollo (Graph QL)",
+      "Socket.IO",
+      "Pusher",
+      "Fire base",
+    ],
   },
   {
     title: "Auth",
-    values: ["Next-Auth", "Clerk", "Fire base", "JWT"]
+    values: ["Next-Auth", "Clerk", "Fire base", "JWT"],
   },
   {
     title: "Test",
-    values: ["Jest", "Cypress", "Playwright"]
+    values: ["Jest", "Cypress", "Playwright"],
   },
   {
     title: "Monorepo",
-    values: ["Lerna", "Turbo"]
+    values: ["Lerna", "Turbo"],
   },
   {
     title: "Others",
-    values: ["tRPC", "Immer", "Rxjs", "Git"]
-  }
-]
+    values: ["tRPC", "Immer", "Rxjs", "Git", "OpenAI API"],
+  },
+];
 const LIBS2 = ["Nginx", "Ngrok", "Docker", "CI/CD", "Express", "Prisma"];
 
 export function Tech() {
@@ -61,7 +74,10 @@ export function Tech() {
         <Typography color="blue-gray" className="mb-8 text-xl font-semibold">
           Frameworks That I've Used
         </Typography>
-        <div className="flex flex-wrap items-center justify-center gap-10" data-aos="fade-up">
+        <div
+          className="flex flex-wrap items-center justify-center gap-10"
+          data-aos="fade-up"
+        >
           {FRAMEWORKS.map((logo, key) => (
             <Image
               key={key}
@@ -79,29 +95,33 @@ export function Tech() {
           Libraries That I've Worked with
         </Typography>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-y-4 ">
-
-          {LIBS.map(el => {
-            return <div key={el.title} className=" flex flex-col xl:ml-16 sm:ml-10 justify-start items-start" >
-              <Typography className=" font-sans !text-gray-800 font-semibold text-md ">
-                {el.title} :
-              </Typography>
-              <ul className="list-disc  flex flex-col justify-start items-start">
-                {el.values.map(subEl => <li key={subEl} className="ml-4">
-                  <Typography className=" font-sans !text-gray-800 ">
-                    {subEl}
-                  </Typography>
-                </li>)}
-
-              </ul>
-            </div>
-
+          {LIBS.map((el) => {
+            return (
+              <div
+                key={el.title}
+                className=" flex flex-col xl:ml-16 sm:ml-10 justify-start items-start"
+              >
+                <Typography className=" font-sans !text-gray-800 font-semibold text-md ">
+                  {el.title} :
+                </Typography>
+                <ul className="list-disc  flex flex-col justify-start items-start">
+                  {el.values.map((subEl) => (
+                    <li key={subEl} className="ml-4">
+                      <Typography className=" font-sans !text-gray-800 ">
+                        {subEl}
+                      </Typography>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            );
           })}
         </div>
         <Typography className="px-8 mt-8 font-sans !text-gray-800  font-semibold text-xl">
           And I am always open to learn more...
         </Typography>
-      </div >
-    </section >
+      </div>
+    </section>
   );
 }
 
